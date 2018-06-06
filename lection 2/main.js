98d6c5103ad31e1729eaaa07a8fbf1f9ffe305bd
Andrey <39730926+AndreyGuzhvii@users.noreply.github.com>
2018-06-06 11:28:28 +0300

    Add files via upload

---
+++ lection 2/main.js
@@ -0,0 +1,58 @@
+/*  n=+prompt('input "n"','');
+
+a=0;
+b=1;
+for(i=2; i<=n; i++){
+    c=a+b;
+    a=b;
+    b=c;
+    
+    console.log(c);
+} */
+
+
+
+
+///////// task2
+
+/* a=+prompt('input a','');
+b=+prompt('input b','');
+h=+prompt('input h','');
+
+fact=1;
+sum=0;
+for(i=a; i<=b;i+=h){
+  
+    console.log(i);
+    
+    fact*=i*(i-1);
+    
+    console.log(fact);
+    
+    sum+=fact;
+} 
+console.log(sum);  */
+
+
+////////Task3
+
+
+
+/* a=0;
+b=100;
+even='';
+for(i=a; i<=b; i++) {
+   
+    if(i%2==0) {
+    even+=i+'\n';
+    
+    }
+}console.log(even);
+
+document.write('<table border="1" style="border: solid; text-align: center">');
+
+document.write('<tr><td>'+ even +'</td></tr><br/>');
+
+document.write('</table>'); */
+
+
